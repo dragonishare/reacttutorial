f10ac421a5fcc94c8c0eb0abbcbd552caba5ef5c
Dragon <dragonishare@gmail.com>
2018-08-02 14:44:12 +0800

    no message

--- demos/lifecycle/src/App.js
+++ demos/lifecycle/src/App.js
@@ -4,11 +4,21 @@ import './App.css';
 
 let a = 'fdsfs';
 const b = x => x * 2;
+const Item = item => {
+  return <div {...item}>hhh</div>;
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Item
+            key="1"
+            name="fsdfds"
+            color="fsdfds"
+            age="ffsdfds"
+            text="fsdfdsf"
+          />
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">
             Welcome to React
