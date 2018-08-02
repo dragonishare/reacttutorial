cf155d40e1e5ab5d28589d8553cb0908fc6b0a47
Dragon <dragonishare@gmail.com>
2018-08-02 14:19:50 +0800

    自动格式化测试，没有添加.prettierrc文件使用prettier默认推荐

--- demos/lifecycle/src/App.js
+++ demos/lifecycle/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-let a = 'fdsfs';
+let a = "fdsfs";
 class App extends Component {
   render() {
     return (
